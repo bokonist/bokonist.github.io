ee4d684c43970e061adbc07c065d9b98c7883f98
bokonist <jayrm1995@gmail.com>
2021-03-29 20:15:22 +0530

    added icon, fixed most of the messy code

--- etch-a-sketch/scripts/main.js
+++ etch-a-sketch/scripts/main.js
@@ -9,34 +9,36 @@ const CURRENT_COLOR = document.querySelector('#current-color');
 const canvas = document.querySelector('#canvas');
 const ctx = canvas.getContext('2d');
 
-let currentBrushColor = 'black';
-CURRENT_COLOR.style['background-color']= currentBrushColor;
-
-let randomModeON = true;
-
-
+let currentBrushColor;
+let randomModeON;
+let eraserModeON;
 let GRID_SIZE=50;
 let cellSize= 500/GRID_SIZE;
 
 //stuff for the custom cursor
 const MOUSE_CURSOR= document.querySelector('.cursor');
-window.addEventListener('mousemove', cursor);
-function cursor (e)
+window.addEventListener('mousemove', cursorTrack);
+function cursorTrack (e)
 {
-    MOUSE_CURSOR.style.top = e.pageY + "px";
-    MOUSE_CURSOR.style.left = e.pageX + "px";
+    MOUSE_CURSOR.style.top = `${e.pageY}px`;
+    MOUSE_CURSOR.style.left = `${e.pageX}px`;
 }
 
+//function to drawInitial grid and set initial default values
 function drawInitial()
 {
-    MAIN_GRID.innerHTML=''; //clear the grid if it already exists
+    MAIN_GRID.innerHTML=''; //clear the main grid if it already exists
+    randomModeON = true;
+    setOutline(true);
+    setCurrentBrush("random", OUTLINE_BUTTON.value);
     cellSize= 500/GRID_SIZE;
+    let CURRENT_ROW, cell;
     //populate the main grid with cells for drawing
     for (let i=0; i< GRID_SIZE; i++){
-        const CURRENT_ROW = document.createElement('div');
+        CURRENT_ROW = document.createElement('div');
         CURRENT_ROW.classList.add('row');
         for (let j=0; j< GRID_SIZE; j++){
-            let cell = document.createElement('div');
+            cell = document.createElement('div');
             cell.classList.add('cell');
             cell.setAttribute('row', j);
             cell.setAttribute('column', i);
@@ -55,8 +57,8 @@ function drawInitial()
     }
 }
 drawInitial();
-//funtion to color a cell and the corresponding position on canvas on the side
-console.log(canvas.height, canvas.width);
+
+//funtion to color a cell and the corresponding position on canvas element on the side
 function colorCell (event) {
 
     if(randomModeON)
@@ -70,6 +72,13 @@ function colorCell (event) {
         event.target.style['background-color']= currentBrushColor;
         ctx.fillStyle=currentBrushColor;
         ctx.fillRect(event.target.getAttribute('column')*cellSize, event.target.getAttribute('row')*cellSize, cellSize, cellSize);
+    }
+    else if (eraserModeON) {
+        MOUSE_CURSOR.style['background-color']=currentBrushColor;
+        event.target.style['background-color']= currentBrushColor;
+        ctx.fillStyle=currentBrushColor;
+        //hacky piece of code to erase just a few pixels more to accomodate for the border. 
+        ctx.fillRect((event.target.getAttribute('column')*cellSize)-1, (event.target.getAttribute('row')*cellSize)-1, cellSize+2, cellSize+2);  
     }
     else{
         MOUSE_CURSOR.style['background-color']=currentBrushColor;
@@ -87,6 +96,59 @@ function colorCell (event) {
     
 }
 
+function setCurrentBrush (brushColor, brushStyle) //brushStyle is true/false for OUTLINE preference 
+{
+    if(brushColor === "random")
+    {
+        randomModeON = true;
+        CURRENT_COLOR.style['background-color']='black'; 
+        CURRENT_COLOR.style['border-top']= '5px solid red';
+        CURRENT_COLOR.style['border-right']= '5px solid green';
+        CURRENT_COLOR.style['border-bottom']= '5px solid blue';
+        CURRENT_COLOR.style['border-left']= '5px solid yellow';
+        MOUSE_CURSOR.style['border-top']= '2px solid red';
+        MOUSE_CURSOR.style['border-right']= '2px solid green';
+        MOUSE_CURSOR.style['border-bottom']= '2px solid blue';
+        MOUSE_CURSOR.style['border-left']= '2px solid yellow';
+    }
+    else {
+        randomModeON= false;
+        eraserModeON=false;
+        MOUSE_CURSOR.style.border= '2px solid black';
+        switch (brushColor) {
+            case "red":
+                currentBrushColor='red';
+                break;
+            case "green":
+                currentBrushColor='green';
+                break;
+            case "yellow":
+                currentBrushColor='yellow';
+                break;
+            case "blue":
+                currentBrushColor='blue';
+                break;
+            case "black":
+                currentBrushColor='black';
+                break;
+            case "eraser":
+                currentBrushColor='#c7c7c7';
+                eraserModeON=true;
+                setOutline(false);
+                break;
+            default: // color was picked with random color picker
+                currentBrushColor= brushColor;
+                break;
+        }
+        MOUSE_CURSOR.style['background-color']=currentBrushColor;
+        CURRENT_COLOR.style['background-color']=currentBrushColor;
+        if(brushColor !== 'eraser' && brushStyle)
+            CURRENT_COLOR.style.border= '5px solid black';
+        else
+            CURRENT_COLOR.style.border= 'none';
+    }
+}
+
 //function to reset canvas and main grid to initial state
 RESET_BUTTON.addEventListener ('click', (e) => {
     for (let i of document.querySelectorAll('.cell'))
@@ -97,16 +159,13 @@ RESET_BUTTON.addEventListener ('click', (e) => {
         ctx.fillStyle='#c7c7c7';
         ctx.fillRect(0,0,GRID_SIZE*cellSize, GRID_SIZE*cellSize);
     }
+    drawInitial();
 })
 
 //function to generate random image. basically simulates a mouseover event for all cells
 RANDOM_BUTTON.addEventListener('click', (e) => {
     randomModeON=true;
-    CURRENT_COLOR.style['background-color']='black'; 
-    CURRENT_COLOR.style['border-top']= '5px solid red';
-    CURRENT_COLOR.style['border-right']= '5px solid green';
-    CURRENT_COLOR.style['border-bottom']= '5px solid blue';
-    CURRENT_COLOR.style['border-left']= '5px solid yellow';
+    setCurrentBrush("random", OUTLINE_BUTTON.value);
     let canvasNodes = document.querySelectorAll('.cell');
     for (let i of canvasNodes){
         let mouseoverEvent = new Event('mouseover');
@@ -125,72 +184,45 @@ DRAW_GRID.addEventListener('click', (e)=> {
         
 })
 
-OUTLINE_BUTTON.addEventListener('click', (e) => {
+function toggleOutline (event)
+{
     if((OUTLINE_BUTTON.value === "false")) // type issue, also solved with == instead of ===
     {
-        OUTLINE_BUTTON.style['background-color']= 'green';
-        
-        OUTLINE_BUTTON.value=true;
-        OUTLINE_BUTTON.innerText="TURN OUTLINE OFF"
-        MOUSE_CURSOR.style.border="3px solid black";
-
-        console.log(OUTLINE_BUTTON.value);
+        setOutline(true);
     }
     else{
-        OUTLINE_BUTTON.value=false;
-        OUTLINE_BUTTON.style['background-color']= 'red';
-        OUTLINE_BUTTON.innerText="TURN OUTLINE ON"
-        MOUSE_CURSOR.style.border="1px solid black";
-        console.log(OUTLINE_BUTTON.value);
+        setOutline(false);
     }
-    
-})
+}
+function setOutline(preference)
+{
+    if(preference === true)
+    {
+        OUTLINE_BUTTON.style['background-color']= 'red';
+        OUTLINE_BUTTON.innerText="TURN OUTLINE OFF";
+        if(randomModeON)
+        {
+            CURRENT_COLOR.style['border-top']= '5px solid red';
+            CURRENT_COLOR.style['border-right']= '5px solid green';
+            CURRENT_COLOR.style['border-bottom']= '5px solid blue';
+            CURRENT_COLOR.style['border-left']= '5px solid yellow';
+        }
+        else
+            CURRENT_COLOR.style.border='5px solid black';
+    }
+    else {
+        OUTLINE_BUTTON.style['background-color']= 'green';
+        OUTLINE_BUTTON.innerText="TURN OUTLINE ON";
+        CURRENT_COLOR.style.border='none';
+    }
+    OUTLINE_BUTTON.value=preference;
+}
+OUTLINE_BUTTON.addEventListener('click',toggleOutline);
 
 // function to switch brush color, and turn outline on if the user wants to
 for (let i of document.querySelectorAll('.palette-button')) {
-
     i.addEventListener('click', (e) => {
-        switch (e.target.getAttribute('id')) {
-            case 'eraser-button':
-                //eraser blocks must have no outline
-                OUTLINE_BUTTON.value=false;
-                OUTLINE_BUTTON.style['background-color']='red';
-                OUTLINE_BUTTON.innerText="TURN OUTLINE ON";
-                CURRENT_COLOR.style['border']= "0px";
-                currentBrushColor= e.target.getAttribute('value');
-                CURRENT_COLOR.style['background-color']= currentBrushColor;
-                MOUSE_CURSOR.style['background-color']=currentBrushColor;
-                randomModeON=false;
-                break;
-            case 'outline-button': //toggle outline ON/OFF
-                if(OUTLINE_BUTTON.value=== "false")
-                {
-                    CURRENT_COLOR.style['border']= "none";
-                }
-                else{
-                    CURRENT_COLOR.style['border']= "4px solid black";
-                }
-                break;
-            case 'random-button': 
-                randomModeON = true;
-                CURRENT_COLOR.style['background-color']='black'; 
-                CURRENT_COLOR.style['border-top']= '5px solid red';
-                CURRENT_COLOR.style['border-right']= '5px solid green';
-                CURRENT_COLOR.style['border-bottom']= '5px solid blue';
-                CURRENT_COLOR.style['border-left']= '5px solid yellow';
-                break;
-            
-            default:
-                randomModeON = false;
-                currentBrushColor= e.target.getAttribute('value');
-                MOUSE_CURSOR.style['background-color']=currentBrushColor;
-                CURRENT_COLOR.style['background-color']= currentBrushColor;
-                if(CURRENT_COLOR.style['border']== "none")
-                    CURRENT_COLOR.style['border']= "4px solid black";
-                break;
-        }
-        console.log(COLOR_PICKER.value);
-        
+       setCurrentBrush(e.target.value, OUTLINE_BUTTON.value);
     });
 }
 //funtions to fire when a new color is picked with color picker, or just clicked and no new value is picked
@@ -229,7 +261,15 @@ function downloadSketch ()
     let dataURL = canvas.toDataURL();
     document.querySelector('#download-button').setAttribute('href',dataURL);
 }
+
+//functions for grid size adjustor
 GRID_ADJUSTOR.addEventListener('mouseup', (e) => {
     GRID_SIZE =  Number(e.target.value);
+    document.querySelector('#grid-size-label').innerText= `${GRID_SIZE} X ${GRID_SIZE}`;
     drawInitial();
+})
+
+GRID_ADJUSTOR.addEventListener('input', (e) => {
+    GRID_SIZE =  Number(e.target.value);
+    document.querySelector('#grid-size-label').innerText= `${GRID_SIZE} X ${GRID_SIZE}`;
 })
